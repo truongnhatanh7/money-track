3521e88c6e42f3f2eab1fc1bcd96c74bb46d4670
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-06-25 17:04:09 +0700

    Initial Commit

---
+++ money-track/money_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_trackApp.swift
+//  money-track
+//
+//  Created by Truong Nhat Anh on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct money_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
